d3494071438ab2dbebd8e51938f9dbaf93caee0f
LoneCuriosity <ramon.garcia08@utrgv.edu>
2022-02-04 22:46:31 +0000

    code refactor

---
+++ Telemetry/Mission Control/missionControl/functions.js
@@ -0,0 +1,30 @@
+function drawOverlay(ctx, canvas, radius) {
+    ctx.restore();
+    ctx.clearRect(-canvas.width, -canvas.height, canvas.width*2, canvas.height*2);
+    var ang;
+    var num;
+    ctx.fillStyle = 'red';
+    ctx.font = radius*0.1 + "px arial";
+    ctx.textBaseline="end";
+    ctx.textAlign="center";
+    
+    for(num = 1; num < 21; num++){
+      ang = num * Math.PI / 10;
+      ctx.rotate(ang);
+      ctx.translate(0, -radius*0.85);
+      ctx.fillRect(0, 0, 3, 15)
+      ctx.translate(0, radius*0.85);
+      ctx.rotate(-ang);
+    }
+  
+    for(num = 1; num < 21; num++){
+      ang = num * Math.PI / 10;
+      ctx.rotate(ang);
+      ctx.translate(0, -radius*0.88);
+      ctx.fillText((num*18).toString(), 0, 0);
+      ctx.translate(0, radius*0.88);
+      ctx.rotate(-ang);
+    }
+  }
+
+  export default drawOverlay
---
+++ Telemetry/Mission Control/missionControl/rocket.js
@@ -0,0 +1,105 @@
+import * as THREE from 'three';
+import drawOverlay from './functions';
+import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader'
+
+export default class Rocket {
+    constructor(canvas, overlayCanvas) {
+        this.canvas = canvas
+        this.overlayCanvas = overlayCanvas
+        this.port = null;
+        this.reader = null;
+        var _self = this;
+
+        this.overlayCanvas.addEventListener('click', async function() {
+            _self.port = await navigator.serial.requestPort()
+            await _self.port.open({ baudRate: 115200 });
+            let textDecoder = new TextDecoderStream();
+            const readableStreamClosed = _self.port.readable.pipeTo(textDecoder.writable);
+            _self.reader = textDecoder.readable.getReader();
+        })
+    }
+
+    async InitViewport() {
+        this.scene = new THREE.Scene();
+
+        this.camera = new THREE.PerspectiveCamera(75, (window.innerWidth / 4) / (window.innerHeight / 2), 0.1, 1000 );
+    
+        this.renderer = new THREE.WebGL1Renderer({
+            canvas: this.canvas,
+        });
+    
+        this.renderer.setPixelRatio( window.devicePixelRatio );
+        this.renderer.setSize( window.innerWidth / 4, window.innerHeight / 2 );
+    
+        this.camera.position.setZ(30);
+    
+        const material = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true })
+    
+        const objLoader = new OBJLoader()
+        this.obj = await objLoader.loadAsync('./model/rocket.obj')
+
+        this.obj.traverse(function (child) {
+            if ((child).isMesh) {
+                (child).material = material
+            }
+        })
+    
+        this.scene.add(this.obj)
+        this.renderer.render( this.scene, this.camera );
+    }
+
+    async InitOverlay() {
+        this.overlayCanvas.width = this.canvas.width;
+        this.overlayCanvas.height = this.canvas.height;
+        this.overlayCanvasContext = this.overlayCanvas.getContext("2d");
+
+        this.radius = this.overlayCanvas.height / 2;
+        this.overlayCanvasContext.translate(this.radius, this.radius);
+        //this.radius = this.radius * 0.90
+
+        this.rawData = []
+        this.ang = 0;
+    }
+
+    async animate() {
+        requestAnimationFrame(this.animate.bind(this));
+        drawOverlay(this.overlayCanvasContext, this.canvas, this.radius)
+      
+        this.overlayCanvasContext.rotate(this.ang);
+        this.overlayCanvasContext.translate(0, -this.radius*0.85);
+        this.overlayCanvasContext.fillRect(0, 0, 3, 15)
+        this.overlayCanvasContext.translate(0, this.radius*0.85);
+        this.overlayCanvasContext.rotate(-this.ang);
+
+        if(this.port && this.reader && this.obj){
+          const { value, done } = await this.reader.read();
+      
+          let InitCharFound = false
+          let TempString = ""
+      
+          value.split("").forEach(char => {
+            if(InitCharFound){
+              if(char == "@"){
+                this.rawData = TempString.split(":").map(val => (val * (Math.PI/180)) )
+                InitCharFound = false
+                TempString = ""
+              } else {
+                TempString += char
+              }
+            }
+      
+            if(char == "#")
+              InitCharFound = true
+          });
+        }
+      
+        if(this.rawData.length > 0){
+          this.obj.rotation.y = this.rawData[2];
+          this.obj.rotation.x = -this.rawData[0] + (Math.PI/180 * 90);
+          this.obj.rotation.z = -this.rawData[1];
+          this.ang = Math.round(this.rawData[1] * 180/Math.PI) * Math.PI / 180;
+        }
+      
+        this.renderer.render( this.scene, this.camera );
+    }
+  }
