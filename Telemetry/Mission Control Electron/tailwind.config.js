cc6d039194d6bee955301dc9e9f2a644a63dd2a1
LoneCuriosity <ramon.garcia08@utrgv.edu>
2022-03-24 23:35:13 -0500

    Mission Control Electron

---
+++ Telemetry/Mission Control Electron/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'Quantico': 'Quantico'
+      }
+    },
+  },
+  plugins: [
+    require('./node_modules/tailwind-percentage-heights-plugin')(),
+  ],
+}
